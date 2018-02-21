cd58dcba70143f4b9c241a629179a766df77a19f
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-21 23:24:18 +0100

    Add ingedient

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,3 +1,28 @@
 class IngredientsController < ApplicationController
 
+  get '/ingredients/add' do
+    if logged_in?
+      erb :'/ingredients/create_ingredient'
+    end
+  end
+
+  post '/ingredients' do
+    if logged_in?
+      if !Ingredient.find_by(name: params[:name])
+        Ingredient.create(name: params[:name])
+      end
+      redirect '/ingredients'
+    else
+      redirect '/recipes'
+    end
+  end
+
+  get '/ingredients' do
+    if logged_in?
+      @ingredients = Ingredient.all
+      erb :'/ingredients/ingredients'
+    end
+  end
+
+
 end
